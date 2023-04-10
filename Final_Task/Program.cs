0f4c7f8d40af06596270f1f43f716c5d74167f15
Aleksandr Kursov <alexander.kursov@gmail.com>
2023-04-10 19:53:40 +0500

    added solution fot Final_Task

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -6,3 +6,53 @@
 // ["hello","2","world",":-)"]->["2",":-)"]
 // ["1234","1567","-2","computer science"]->["-2"]
 // ["Russia","Denmark","Kazan"]->[]
+
+string[] InputArray(string text)
+{
+    System.Console.Write(text);
+    return Console.ReadLine()!.Split(" ");
+}
+
+void PrintArray(string[] arr)
+{
+    System.Console.WriteLine("[\"" + string.Join("\",\"", arr) + "\"]");
+}
+
+int FindingLengthNewArray(string[] arr)
+{
+    int lengthNewArray = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            lengthNewArray++;
+        }
+    }
+    return lengthNewArray;
+}
+
+string[] ArraySorting(string[] arr, int size)
+{
+    string[] sortedArray = new string[size];
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            sortedArray[index] = arr[i];
+            index++;
+        }
+    }
+    return sortedArray;
+}
+
+System.Console.WriteLine();
+string[] myArray = InputArray("Enter a space-separated string array: ");
+int lengthNewArray = FindingLengthNewArray(myArray);
+System.Console.WriteLine();
+System.Console.Write("Initial array: ");
+PrintArray(myArray);
+System.Console.WriteLine();
+System.Console.Write("Sorted array: ");
+PrintArray(ArraySorting(myArray, lengthNewArray));
+System.Console.WriteLine();
